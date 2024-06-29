bf69fbca5d59b93e365c123cb5be31b1613abbd9
Sundara Rao <70574120+sundarp1438@users.noreply.github.com>
2024-06-29 09:16:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "aks" {
 # Create a virtual network
 resource "azurerm_virtual_network" "aks" {
   name                = "aksVnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
 }
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "aks" {
   name                 = "aksSubnet"
   resource_group_name  = azurerm_resource_group.aks.name
   virtual_network_name = azurerm_virtual_network.aks.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 }
 
 # Create an AKS cluster
