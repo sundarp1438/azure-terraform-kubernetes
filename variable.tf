313954b2b62e0bb4184e7caebf59b5c3082d644c
Sundara Rao <70574120+sundarp1438@users.noreply.github.com>
2024-06-29 08:52:10 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,39 +1,3 @@
-# resource group name
-variable "resource_group_name" {
-  type        = string
-  description = "Resource Group name"
-}
-
-# region
-variable "region" {
-  type        = string
-  description = "Region"
-}
-
-# environment
-variable "environment" {
-  type        = string
-  description = "Environment"
-}
-
-# max node count 
-variable "max_node_count" {
-  type        = number
-  description = "Maximun node count for worker node"
-}
-
-# min node count 
-variable "min_node_count" {
-  type        = number
-  description = "Minimum node count for worker node"
-}
-
-# size of worker node
-variable "node_vm_size" {
-  type        = string
-  description = "Size of worker node"
-}
-
 # client ID
 variable "client_id" {
   type = string
