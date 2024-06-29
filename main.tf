e87b3cd59ccfb7ad3d4dc0fc7beb5d25f8395808
Sundara Rao <70574120+sundarp1438@users.noreply.github.com>
2024-06-29 08:35:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_DS2_v2"
     vnet_subnet_id = azurerm_subnet.aks.id
   }
