8182847590e9f671367daabafb421916f5a278a7
Sundara Rao <70574120+sundarp1438@users.noreply.github.com>
2024-06-29 08:44:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     client_id     = "73e7d17d-9f8a-40a0-a173-102b80347bcf"
     client_secret = "3yw8Q~RMnk5ZPZo1A~0rxfKtOt0NYOaQdfjNsc0a"
   }
-}
+
   identity {
     type = "SystemAssigned"
   }
